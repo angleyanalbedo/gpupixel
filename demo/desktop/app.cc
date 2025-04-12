f4d52a0aba0ddcb837cdadb4b996ff0a11d83581
Jaaron <jaaronkot@gmail.com>
2025-04-12 17:47:50 +0800

    111

--- demo/desktop/app.cc
+++ demo/desktop/app.cc
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include "gpupixel.h"
+#include "gpupixel/gpupixel.h"
 #include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
